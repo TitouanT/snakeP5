2fbefe941ee5103254872e72418f403197a36fd3
Titouan Teyssier <titouan.teyssier@gmail.com>
2016-06-05 04:28:54 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ var food;
 var heightGrid;
 var widthGrid;
 function setup() {
-    createCanvas (1000, 500);
+    createCanvas (600, 400);
     heightGrid = int(height/scl);
     widthGrid = int(width/scl);
     s = new Snake();
