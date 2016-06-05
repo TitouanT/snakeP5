35156f172f911e7259276c37e9e43e76271e22f6
Titouan Teyssier <titouan.teyssier@gmail.com>
2016-06-05 04:00:17 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var s;
+var scl = 20;
+var i;
+var speed = 2;
+var food;
+var heightGrid;
+var widthGrid;
+function setup() {
+    createCanvas (600, 300);
+    heightGrid = int(height/scl);
+    widthGrid = int(width/scl);
+    s = new Snake();
+    i = 0;
+    food = createVector(int(random(widthGrid))*scl, int(random(heightGrid))*scl);
+}
+
+function draw() {
+    background(51);
+    if (!s.alive) {
+        s.total = 0;
+        s.alive = true;
+    }
+    if (i == 0) {
+        if (s.eat(food)) food = createVector(int(random(widthGrid))*scl, int(random(heightGrid))*scl);
+        s.update();
+        s.movementSet = false;
+    }
+    
+    fill (255, 0, 0, 200);
+    rect (food.x, food.y, scl, scl);
+    s.show();
+    i++;
+    if (i > speed) i = 0;
+}
+
+function keyPressed() {
+    if (keyCode === UP_ARROW) {
+        s.dir(0, -1);
+    } else if (keyCode === DOWN_ARROW) {
+        s.dir(0, 1);
+    } else if (keyCode === RIGHT_ARROW) {
+        s.dir(1, 0);
+    } else if (keyCode === LEFT_ARROW) {
+        s.dir(-1, 0);
+    }
+}
---
+++ snake.js
@@ -0,0 +1,62 @@
+function Snake () {
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+    this.movementSet = false;
+    this.justEat = false;
+    this.total = 0;
+    this.tail = [];
+    this.alive = true;
+    
+    this.dir = function (x, y) {
+        if (abs(x) != abs(this.xspeed) && !this.movementSet) {
+            this.xspeed = x;
+            this.yspeed = y;
+            this.movementSet = true;
+        } 
+        //if (abs(y) != abs(this.yspeed)) this.yspeed = y;
+    }
+    
+    this.eat = function (pieceOf) {
+        if (pieceOf.x == this.x && pieceOf.y == this.y) {
+            this.total++;
+            this.justEat = true;
+            return true;
+        }
+        return false;
+    }
+    
+    this.update = function() {
+        if (!this.justEat) {
+            for (var i = 0; i < this.total - 1; i++) {
+                this.tail[i] = this.tail[i+1];
+            }
+            
+        }
+        else this.justEat = false;
+        this.tail[this.total - 1] = createVector(this.x, this.y);
+        this.x = this.x + this.xspeed*scl;
+        this.y = this.y + this.yspeed*scl;
+        
+        if (this.x > width - scl) this.x = 0;
+        else if (this.x < 0) this.x = width - scl;
+        
+        if (this.y > height - scl) this.y = 0;
+        else if (this.y < 0) this.y = height - scl;
+        for (var i = 0; i < this.total; i++) {
+            if (this.tail[i].x == this.x && this.tail[i].y == this.y)
+                this.alive = false;
+        }
+    }
+    
+    this.show = function() {
+        if (this.alive) fill(255, 200);
+        else fill (0);
+        for (var i = 0; i < this.total; i++) {
+            if (this.justEat) console.log("PROBLEME");
+            rect(this.tail[i].x, this.tail[i].y, scl, scl);
+        }
+        rect(this.x, this.y, scl, scl);
+    }
+}
